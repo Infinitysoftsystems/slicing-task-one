3ab12afa2d8328cdc5e622899572d72582809b4d
Mukteshwar <69595831+mcreative1988@users.noreply.github.com>
2021-02-24 23:38:34 +0530

    HTML updation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,31 +6,22 @@ import thumb from '../../assets/img/thumb.jpg'
 const Home = () => {
   return (
     <div class="container-fluid  bg-grey ">    
-  
-    <div class="row d-flex mainBox">
-      
-      <div class="col-9 text-left bg-white h-100 px-0"> 
-            {
-                //Header is a reuable component and will be integrated in routes in future purpous.
-            }
-          <Header />
-         <div class=" bg-3 py-2">
-             <div class="col-12">
-                  <button class="reloadBtn mr-2"><i class="demo-icon icon-arrows-cw"></i>RELOAD SCHEDULER</button>
-                  <button class="resetBTn"><i class="demo-icon icon-recycle"></i>RESET DATABASE</button>
-             </div>
-         </div>
-         <div class="row">
-              <div class="col-12">
-                  <img class="w-100" src={thumb} />
-              </div>
-         </div>
-      </div>
-      {
+    <div class="row">
+	<div class="col-9 px-0">
+		{
+			//Header is a reuable component and will be integrated in routes in future purpous.
+		}
+	  <Header />
+	</div>
+	<div class="col-3 bg-2 text-white font-weight-normal">
+		Side panel
+	</div>
+</div>
+    
+    {
           //Scheduler is also our reuable component and intetrated from ui-components
       }
       <Scheduler />
-    </div>
   </div>
   );
 }
--- src/ui-components/Scheduler/Scheduler.js
+++ src/ui-components/Scheduler/Scheduler.js
@@ -45,45 +45,97 @@ const Scheduler = () => {
 	const [tasks, setTasks] = useState(allTasks);
 	
 	return (
-		<div class="col-3 sidenav h-100">
-			<div class="row">
-				<div class="col-12 bg-2 text-white font-weight-normal">
-					Side panel
+		<div class="row d-flex mainBox">
+    
+		<div class="col-9 text-left bg-white h-100 px-0"> 
+			<div class="col-12 bg-3 py-2">
+				<button class="reloadBtn mr-3"><i class="demo-icon icon-arrows-cw"></i>RELOAD SCHEDULER</button>
+				<button class="resetBTn"><i class="demo-icon icon-recycle"></i>RESET DATABASE</button>
 			</div>
-			</div>
-
-			<div class="row">
-				<div class="col-12">
-					<ul class="list-unstyled taskTabs py-3">
-						<li>
-							<a href="javascript:void();" class="d-flex justify-content-between">
-								<span class="taskInfo"> Task 1</span>
-								<span class="timeLaps font-weight-bold">2h</span>
-							</a>
-						</li>
-						<li>
-							<a href="javascript:void();" class="d-flex justify-content-between">
-								<span class="taskInfo"> Task 2</span>
-								<span class="timeLaps font-weight-bold">3h</span>
-							</a>
-						</li>
-						<li>
-							<a href="javascript:void();" class="d-flex justify-content-between">
-								<span class="taskInfo"> Task 3</span>
-								<span class="timeLaps font-weight-bold">1h</span>
-							</a>
-						</li>
-						<li>
-							<a href="javascript:void();" class="d-flex justify-content-between">
-								<span class="taskInfo"> Task 4</span>
-								<span class="timeLaps font-weight-bold">4h</span>
-							</a>
-						</li>
-					</ul>
+			
+			<div class="col-12 border border-left-0 bg-3">
+				<div class="row d-flex align-items-center shadow-sm">
+					<div class="col-2">
+						Name
+					</div>
+					<div class="col-10 text-center border-left">
+						<div>MON 05/21</div>
+						<div class="row d-lfex justify-content-between  border-top timeDiv">
+							<div class="col-1">6 AM</div> 
+							<div class="col-1">7 AM</div> 
+							<div class="col-1">8 AM</div> 
+							<div class="col-1">9 AM</div> 
+							<div class="col-1">10 AM</div> 
+							<div class="col-1">11 AM</div> 
+							<div class="col-1">12 PM</div> 
+							<div class="col-1">1 PM</div> 
+							<div class="col-1">2 PM</div> 
+							<div class="col-1">3 PM</div> 
+							<div class="col-1">4 PM</div> 
+							<div class="col-1">5 PM</div> 
+						</div>
+					</div>
 				</div>
 			</div>
-
+			
+			
+			<div class="col-12 border-bottom">
+				<div class="row">
+					<div class="col-2 py-2">
+						Car
+					</div>
+					<div class="col-10 text-center border-left  border-right  py-2">
+						draggable element
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-12 border-bottom">
+				<div class="row">
+					<div class="col-2 py-2">
+						Car
+					</div>
+					<div class="col-10 text-center border-left  border-right  py-2">
+						draggable element
+					</div>
+				</div>
+			</div>
+				
+			
 		</div>
+    
+		<div class="col-3 sidenav h-100">
+			<div class="row">
+				<div class="col-12  taskTabs py-3">
+					<div> 
+						<a href="javascript:void();"  class="d-flex justify-content-between"> 
+							<span class="taskInfo"> Task 1</span> 
+							<span class="timeLaps font-weight-bold">2h</span>
+						</a>
+					</div>
+					<div> 
+						<a href="javascript:void();"  class="d-flex justify-content-between"> 
+							<span class="taskInfo"> Task 2</span> 
+							<span class="timeLaps font-weight-bold">3h</span>
+						</a>
+					</div>
+					<div> 
+						<a href="javascript:void();"  class="d-flex justify-content-between"> 
+							<span class="taskInfo"> Task 3</span> 
+							<span class="timeLaps font-weight-bold">1h</span>
+						</a>
+					</div>
+					<div> 
+						<a href="javascript:void();"  class="d-flex justify-content-between"> 
+							<span class="taskInfo"> Task 4</span> 
+							<span class="timeLaps font-weight-bold">4h</span>
+						</a>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
 	);
 }
 
